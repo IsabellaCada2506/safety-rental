aef8dd58ddd06d2ce84dec041232953dfc43b8af
unknown <isabellaocampo128@gmail.com>
2026-09-04 19:33:40 -0500

    feat Finish01 - Languague

---
+++ lang/en/general.php
@@ -0,0 +1,24 @@
+<?php
+
+// Autor: Isabella Ocampo Sánchez
+
+return [
+    'app_name' => 'Safety Rental',
+    'nav_home' => 'Home',
+    'nav_cars' => 'Cars',
+    'nav_locations' => 'Locations',
+    'nav_reservations' => 'My Reservations',
+    'nav_profile' => 'Profile',
+    'nav_logout' => 'Log out',
+
+    'save_button' => 'Save',
+    'cancel_button' => 'Cancel',
+    'edit_button' => 'Edit',
+    'delete_button' => 'Delete',
+    'search_placeholder' => 'Search...',
+
+    'success_message' => 'Operation completed successfully.',
+    'error_message' => 'Something went wrong. Please try again.',
+
+    'footer_rights' => 'All rights reserved.',
+];
---
+++ lang/es/general.php
@@ -0,0 +1,24 @@
+<?php
+
+// Autor: Isabella Ocampo Sánchez
+
+return [
+    'app_name' => 'Safety Rental',
+    'nav_home' => 'Inicio',
+    'nav_cars' => 'Carros',
+    'nav_locations' => 'Sedes',
+    'nav_reservations' => 'Mis Reservas',
+    'nav_profile' => 'Perfil',
+    'nav_logout' => 'Cerrar sesión',
+
+    'save_button' => 'Guardar',
+    'cancel_button' => 'Cancelar',
+    'edit_button' => 'Editar',
+    'delete_button' => 'Eliminar',
+    'search_placeholder' => 'Buscar...',
+
+    'success_message' => 'Operación completada con éxito.',
+    'error_message' => 'Algo salió mal. Inténtalo de nuevo.',
+
+    'footer_rights' => 'Todos los derechos reservados.',
+];
